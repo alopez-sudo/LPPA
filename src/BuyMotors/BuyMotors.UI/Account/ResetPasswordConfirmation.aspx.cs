ff02b451842cc7607d77628769a3fdaf29648572
Mariano Jorreto <marianojorreto@gmail.com>
2020-04-04 19:41:57 -0300

    Creación de la solución

---
+++ src/BuyMotors/BuyMotors.UI/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BuyMotors.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ src/BuyMotors/BuyMotors.UI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuyMotors.Startup))]
+namespace BuyMotors
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
