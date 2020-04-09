82d593e183cea2ce054de1c453537dab0ff4712b
Jorge Z <trabajo.codigo@gmail.com>
2020-04-09 16:10:53 -0300

    Actualizando entidades capa BE Agregando web form vehiculos

---
+++ src/BuyMotors/BuyMotors.BE/CategoriaVehiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyMotors.BE
+{
+	public class CategoriaVehiculo
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+	}
+}
--- src/BuyMotors/BuyMotors.BE/Vehiculo.cs
+++ src/BuyMotors/BuyMotors.BE/Vehiculo.cs
@@ -5,8 +5,10 @@
         public int Id { get; set; }
         public string Patente { get; set; }
         public TipoVehiculo Tipo { get; set; }
-        public Color Color { get; set; }
+		public CategoriaVehiculo Categoria { get; set; }
+		public Color Color { get; set; }
         public Modelo Modelo { get; set; }
         public int Precio { get; set; }
+        public int AnioFabricacion { get; set; }
     }
 }
