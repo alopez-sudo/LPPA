4affbdb6bc4249982374ca8a2c6d645c1afb9369
Mariano Jorreto <marianojorreto@gmail.com>
2020-09-02 22:50:58 -0300

    Se agrega un servicio web para traer el listado de vehículos

--- src/BuyMotors/BuyMotors.UI/ListadoVehiculos.aspx.cs
+++ src/BuyMotors/BuyMotors.UI/ListadoVehiculos.aspx.cs
@@ -1,39 +1,55 @@
 ﻿using BuyMotors.BE.Filtros;
-using BuyMotors.BL;
 using System;
 
 namespace BuyMotors.UI
 {
-	public partial class ListadoVehiculos : BasePage
+    public partial class ListadoVehiculos : BasePage
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			if(!IsPostBack)
-				CargarGvVehiculo(null);
-		}
+            if (!IsPostBack)
+            {
+                CargarGvVehiculo(null);
+            }
+        }
 
 		private void CargarGvVehiculo(FiltroVehiculo filtro)
 		{
-			gvVehiculos.DataSource = VehiculoManager.ObtenerVehiculos(filtro);
+			//gvVehiculos.DataSource = VehiculoManager.ObtenerVehiculos(filtro);
+			VehiculosWS vehiculosWS = new VehiculosWS();
+			gvVehiculos.DataSource = vehiculosWS.ObtenerVehiculos(filtro);
 			gvVehiculos.DataBind();
 		}
 
 		protected void BtnBuscar_Click(object sender, EventArgs e)
 		{
-			int unNumero;
-			FiltroVehiculo filtro = new FiltroVehiculo();
-			if(!string.IsNullOrEmpty(TxtPatente.Text))
-				filtro.Patente = TxtPatente.Text;
-			if(!string.IsNullOrEmpty(TxtPrecioDesde.Text) && int.TryParse(TxtPrecioDesde.Text, out unNumero))
-				filtro.PrecioDesde = int.Parse(TxtPrecioDesde.Text);
-			else
-				TxtPrecioDesde.Text = string.Empty;
-			if(!string.IsNullOrEmpty(TxtPrecioHasta.Text) && int.TryParse(TxtPrecioHasta.Text, out unNumero))
-				filtro.PrecioHasta = int.Parse(TxtPrecioHasta.Text);
-			else
-				TxtPrecioHasta.Text = string.Empty;
-			CargarGvVehiculo(filtro);
+            FiltroVehiculo filtro = new FiltroVehiculo();
+            if (!string.IsNullOrEmpty(TxtPatente.Text))
+            {
+                filtro.Patente = TxtPatente.Text;
+            }
+
+            if (!string.IsNullOrEmpty(TxtPrecioDesde.Text) && int.TryParse(TxtPrecioDesde.Text, out _))
+            {
+                filtro.PrecioDesde = int.Parse(TxtPrecioDesde.Text);
+            }
+            else
+            {
+                TxtPrecioDesde.Text = string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(TxtPrecioHasta.Text) && int.TryParse(TxtPrecioHasta.Text, out _))
+            {
+                filtro.PrecioHasta = int.Parse(TxtPrecioHasta.Text);
+            }
+            else
+            {
+                TxtPrecioHasta.Text = string.Empty;
+            }
+
+            CargarGvVehiculo(filtro);
 		}
+
 		protected void BtnLimpiar_Click(object sender, EventArgs e)
 		{
 			TxtPatente.Text = string.Empty;
---
+++ src/BuyMotors/BuyMotors.UI/VehiculosWS.asmx.cs
@@ -0,0 +1,27 @@
+﻿using BuyMotors.BE;
+using BuyMotors.BE.Filtros;
+using BuyMotors.BL;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Services;
+
+namespace BuyMotors.UI
+{
+    /// <summary>
+    /// Descripción breve de VehiculosWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class VehiculosWS : WebService
+    {
+
+        [WebMethod]
+        public IEnumerable<Vehiculo> ObtenerVehiculos(FiltroVehiculo filtro)
+        {
+            return VehiculoManager.ObtenerVehiculos(filtro);
+        }
+    }
+}
