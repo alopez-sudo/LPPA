b18fd078e597098c55c8c16d2d938268d9f2ae8e
Mariano Jorreto <marianojorreto@gmail.com>
2020-10-04 15:40:30 -0300

    Ejercicio XML integrador - Lectura con XmlTextReader

---
+++ src/Ejercicios MCGA/EjercicioIntegradorXml/EjercicioIntegradorXml/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EjercicioIntegradorXml.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Lectura con XmlTextReader</h3>
+            <asp:Button ID="btnLeerConXmlTextReader" runat="server" Text="Leer con XmlTextReader" OnClick="BtnLeerConXmlTextReader_Click" />
+            <br />
+            <div>
+                <asp:Label ID="lblDatos" runat="server"></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ src/Ejercicios MCGA/EjercicioIntegradorXml/EjercicioIntegradorXml/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.UI;
+using System.Xml;
+
+namespace EjercicioIntegradorXml
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLeerConXmlTextReader_Click(object sender, EventArgs e)
+        {
+            XmlTextReader miLector = new XmlTextReader(Server.MapPath("Libros.xml"));
+            bool printText = false;
+            while (miLector.Read())
+            {
+                if (miLector.NodeType == XmlNodeType.Element)
+                {
+                    switch (miLector.Name)
+                    {
+                        case "Libro":
+                            // Es un libro nuevo, agrego un renglón en blanco
+                            lblDatos.Text += "<br/>";
+                            break;
+                        case "NombreLibro":
+                            lblDatos.Text += "<b>Nombre:</b> ";
+                            // Se pone esta variable en true para que se imprima su valor en la próxima iteración del while
+                            printText = true;
+                            break;
+                        case "Autor":
+                            lblDatos.Text += "<b>Autor:</b> ";
+                            printText = true;
+                            break;
+                        case "Editorial":
+                            lblDatos.Text += "<b>Editorial:</b> ";
+                            printText = true;
+                            break;
+                        case "Precio":
+                            lblDatos.Text += "<b>Precio:</b> $";
+                            printText = true;
+                            break;
+                    }
+                }
+                else if (miLector.NodeType == XmlNodeType.Text && printText)
+                {
+                    printText = false;
+                    lblDatos.Text += miLector.Value + "<br/>";
+                }
+            }
+            miLector.Close();
+        }
+    }
+}
