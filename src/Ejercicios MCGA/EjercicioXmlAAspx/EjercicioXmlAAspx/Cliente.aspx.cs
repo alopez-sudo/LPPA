627a77a4a3e972c85a7ca46e7f35002da5b06abd
Mariano Jorreto <marianojorreto@gmail.com>
2020-10-04 11:59:35 -0300

    Ejercicio XML a ASPX

---
+++ src/Ejercicios MCGA/EjercicioXmlAAspx/EjercicioXmlAAspx/Cliente.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+using System.Xml;
+
+namespace EjercicioXmlAAspx
+{
+    public partial class Cliente : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlTextReader xmlTextReader = new XmlTextReader(Server.MapPath("Datos.xml"));
+            Session["Apellido"] = "";
+            Session["Sueldo"] = "";
+            while (xmlTextReader.Read())
+            {
+                xmlTextReader.MoveToElement();
+                if (xmlTextReader.Name == "Apellido")
+                {
+                    Session["Apellido"] = xmlTextReader.ReadString();
+                }
+                if (xmlTextReader.Name == "Sueldo")
+                {
+                    Session["Sueldo"] = xmlTextReader.ReadString();
+                }
+            }
+
+            xmlTextReader.Close();
+            Response.Redirect("VerDatos.aspx");
+        }
+    }
+}
---
+++ src/Ejercicios MCGA/EjercicioXmlAAspx/EjercicioXmlAAspx/VerDatos.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.UI;
+
+namespace EjercicioXmlAAspx
+{
+    public partial class VerDatos : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (float.Parse(Session["Sueldo"].ToString()) > 70000)
+            {
+                Response.Write("Usted es afortunado");
+            }
+            else
+            {
+                Response.Write("Usted es pobre");
+            }
+        }
+    }
+}
